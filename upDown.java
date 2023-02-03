b2edd674a1b279d79a8261875d4f60ed33ed9a1d
ggamangso1 <ggamangso@gmail.com>
2023-02-03 13:50:49 +0900

    connect git with intellij

--- upDown.java
+++ upDown.java
@@ -1,30 +1,32 @@
 // 소스 코드 수정 중
 // 바꿔보자  바꿔봐
-//
+
 package game;
 
 import java.util.Scanner;
+
 // 변화를 일으켜
 public class upDown {
-	public static void main(String[] args) {
-		
-		int answer = (int)(Math.random() * 100 + 1);
-		int guess;
-		int count = 0;
-		Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
 
-		do {
-			System.out.print("정답을 추측하여 보세요 : ");
-			guess = scan.nextInt();
-			count++;
-			
-			if(guess > answer)
-				System.out.println("다운!");
-			if(guess < answer)
-				System.out.println("업!");
-		} while(guess != answer);
-		
-		System.out.println("축하합니다. 시도 횟수는 " + count + " 번 입니다.");
-	}
+        int answer = (int) (Math.random() * 100 + 1);
+        int guess;
+        int count = 0;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("정답을 추측하여 보세요 : ");
+            guess = scan.nextInt();
+            count++;
+
+            if (guess > answer)
+                System.out.println("다운!");
+            if (guess < answer)
+                System.out.println("업!");
+        } while (guess != answer);
+
+        System.out.println("축하합니다. 시도 횟수는 " + count + " 번 입니다.");
+        System.out.println("내가 바꾼 프로그램");
+    }
 
 }
